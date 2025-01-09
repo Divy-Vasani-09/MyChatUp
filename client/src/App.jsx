b986b7fac2858f3aafadbfdc9eba11b993ba8445
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2025-01-09 16:33:44 +0530

    install react router dom and set in app.jsx and set default component as navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import './App.css'
+import Navbar from './component/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar /></>, // Default route
+    },
+    
+  ])
   return (
     <>
-      <div className="container flex justify-between bg-slate-100 text-white py-3 shadow-lg">
-        <header>
-          <div className='bg-slate-300 rounded-2xl p-1 px-2 flex col font-medium mx-2 text-black text-lg'><h1 className='font-bold '>M</h1><p className='text-base italic'>y</p><h1 className='font-bold'>C</h1><p className='italic'>hat</p><h1 className='font-bold'>MX</h1></div>
-        </header>
-      </div>
+      <RouterProvider router={router} />
     </>
   )
 }
