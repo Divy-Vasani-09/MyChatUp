d6e2c4c52c04720b877e24c52b92041a632951ad
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2025-01-09 16:31:17 +0530

    create component file

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="container flex justify-between bg-slate-100 text-white py-3 shadow-lg">
+        <header>
+          <div className='bg-slate-300 rounded-2xl p-1 px-2 flex col font-medium mx-2 text-black text-lg'><h1 className='font-bold '>M</h1><p className='text-base italic'>y</p><h1 className='font-bold'>C</h1><p className='italic'>hat</p><h1 className='font-bold'>MX</h1></div>
+        </header>
+      </div>
+    </div>
+  )
+}
