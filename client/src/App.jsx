1f2b7f49876376fd20c904b37509914f20ab99ff
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2025-01-23 18:33:47 +0530

    shift routing in app.js to main.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './component/Navbar';
-import Registration from './component/registration';
-import Login from './component/login';
-import ForgotPassword from './component/ForgotPassword';
-import DashBoard from './component/DashBoard';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Navbar /><Registration /></>, // Default route
-    },
-    {
-      path: "/Registration",
-      element: <><Navbar /><Registration /></>,
-    },
-    {
-      path: "/Login",
-      element: <><Navbar /><Login /></>,
-    },
-    {
-      path: "/ForgotPassword",
-      element: <><Navbar /><ForgotPassword /></>,
-    },
-    {
-      path: "/DashBoard",
-      element: <><Navbar /><DashBoard /></>,
-    },
-
-  ])
   return (
     <>
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
+      <Navbar/>
+      <Outlet/>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,53 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Registration from './component/registration';
+import Login from './component/login';
+import ForgotPassword from './component/ForgotPassword';
+import DashBoard from './component/DashBoard';
+import Contact from './component/DashBoardComponent/Contact';
+import Profile from './component/DashBoardComponent/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Default route
+    children: [
+      {
+        path: "/registration",
+        element: <Registration />, 
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/ForgotPassword",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "/DashBoard",
+        element: <DashBoard />,
+        children: [
+          {
+            path: "contact",
+            element: <Contact />
+          },
+          {
+            path: "profile",
+            element: <Profile />
+          },
+        ]
+      },
+    ]
+  },
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
