9129bfe93635396bc6c2a5a8989aa5f149cafb82
Divy-Vasani-09 <divyvasani.niqox@gmail.com>
2025-02-11 11:25:34 +0530

    improve stucture of code

--- client/src/component/DashBoardComponent/SearchBarNewChat.jsx
+++ client/src/component/DashBoardComponent/SearchBarNewChat.jsx
@@ -2,25 +2,24 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FaSearch } from 'react-icons/fa'
 
-const SearchBarNewChat = ({setSearchResults}) => {
+const SearchBarNewChat = ({setSearchResults, userData}) => {
     const [input, setInput] = useState("");
-
     useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
+        const delayToCall = setTimeout(() => {
             if (input.trim() !== "") {
-                axios.get('http://127.0.0.1:3002/SearchBarNewChat', { params: { input } })
+                axios.get('http://127.0.0.1:3002/SearchBarNewChat', { params: { input, userData } })
                     .then(result => {
-                        console.log(result)
+                        console.log(result);
                         const searchResult = result.data.user;
-                        setSearchResults(searchResult)
+                        setSearchResults(searchResult);
                     })
                     .catch(err => {
-                        console.log(err)
+                        console.log(err);
                     })
             }
-        }, 1000)
+        }, 800)
 
-        return () => clearTimeout(delayDebounceFn)
+        return () => clearTimeout(delayToCall)
     }, [input])
 
     const searchHandler = (value) => {
@@ -30,7 +29,7 @@ const SearchBarNewChat = ({setSearchResults}) => {
     return (
         <div>
             <div className="container">
-                <div className="container flex px-2 py-1 items-center bg-blue-900 opacity-95 w-full h-9 rounded-lg drop-shadow shadow-md hover:shadow-blue-600 duration-100">
+                <div className="container flex px-2 py-1 items-center bg-slate-950 bg-opacity-90 w-full h-9 rounded-lg drop-shadow shadow-md hover:shadow-slate-400 duration-200">
                     <FaSearch id="Search-icon" className='text-slate-400' />
                     <input
                         type='text'
